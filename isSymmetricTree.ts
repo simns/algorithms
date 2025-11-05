9f2eaa15f05d0a53311743bc0fbcaa14b0051cd6
Simon <simon.luppescu@gmail.com>
2025-11-05 01:50:16 -0500

    Added isSymmetricTree

---
+++ isSymmetricTree.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSymmetric(root: TreeNode | null): boolean {
+    return isSymHelper(root.left, root.right);
+};
+
+function isSymHelper(left: TreeNode | null, right: TreeNode | null): boolean {
+    if (left === null && right === null) return true;
+    if (left === null || right === null) return false;
+    if (left.val !== right.val) return false;
+
+    return isSymHelper(left.left, right.right) && isSymHelper(left.right, right.left);
+}
